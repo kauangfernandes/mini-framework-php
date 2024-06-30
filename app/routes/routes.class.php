13cc76211a32d5d7ded3b27891791534a7d6f630
Kauan Gomes Fernandes <kauangomesfernandespro@gmail.com>
2024-06-29 22:03:23 -0300

    Update class rotas

--- app/routes/routes.class.php
+++ app/routes/routes.class.php
@@ -1,8 +1,41 @@
 <?php
-    namespace App\Routes;
 
-    class routes{
-        protected array $routes = array();
+    class Routes{
+        protected static array $routes = array();
+        protected static string $method = "";
+        protected static string $path = "";
+
+        public static function getRoutes(){
+            return self::$routes;
+        }
+
+        public static function get(string $path, array $dados){
+            self::$routes['GET'][$path] = $dados;
+        }
+
+        public static function post(string $path, array $dados){
+            self::$routes['POST'][$path] = $dados;
+        }
+
+        public static function runRoutes(){
+            self::$method = Server::getMethodHttp();
+            self::$path = Server::getPathHtpp();
+
+            if(isset(self::$routes[self::$method][self::$path])){
+                return self::$routes[self::$method][self::$path];
+            } else {
+                return exit("Rota não conhecida.");
+            }
+        }
+
+        public static function startInstance(){
+            $route = self::runRoutes();
+            $controller = $route[0];
+            $action = $route[1];
+            $controller = new $controller;
+            $controller->$action();
+        }
     }
 
+    require_once "web/web.php";
 ?>

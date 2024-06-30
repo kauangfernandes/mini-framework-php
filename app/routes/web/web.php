540a5609de799520b8bc7f4f9d6a8eba7ac63fc8
Kauan Gomes Fernandes <kauangomesfernandespro@gmail.com>
2024-06-29 21:40:23 -0300

    Criação de rotas http

---
+++ app/routes/web/web.php
@@ -0,0 +1,7 @@
+<?php
+    Routes::get("/", [indexController::class, "index"]);
+
+
+    //Routes::get("/login", [exemploController::class, "login"]);
+    //Routes::post("/login", [exemploController::class, "Altenticar"]);
+?>

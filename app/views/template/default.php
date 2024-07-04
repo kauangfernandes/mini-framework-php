93efcb87475b50a62dfc1a00f80fe8426d15317b
Kauan Gomes Fernandes <kauangomesfernandespro@gmail.com>
2024-07-04 19:31:27 -0300

    Tentando carregar css e js

--- app/views/template/default.php
+++ app/views/template/default.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="app/views/css/style.css">
-    <script src="app/views/js/index.js" defer></script>
+    <link rel="stylesheet" href="../app/css/styles.css">
+    <script src="../app/js/index.js" defer></script>
     
     <title><?php echo $title ?></title>
 </head>
